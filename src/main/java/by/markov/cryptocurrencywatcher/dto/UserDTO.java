565a3b29a94875688902a1447013f9aa59e50ceb
markov vadim <markovadim@gmail.com>
2022-09-25 00:20:59 +0300

    master: fixed compose file

--- src/main/java/by/markov/cryptocurrencywatcher/dto/UserDTO.java
+++ src/main/java/by/markov/cryptocurrencywatcher/dto/UserDTO.java
@@ -4,16 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
 
-    @Size(min = 3, message = "Username cannot be less 3 characters")
     private String username;
 
-    @Size(min = 3, message = "Currency symbol cannot be less 3 characters")
     private String symbol;
 }
